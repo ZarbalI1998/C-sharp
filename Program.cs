cd25e477fd69adc64f53aeacbe8758f4ede51c95
unknown <zarbaliev@code.edu.az>
2022-11-13 22:31:00 -0800

    added new file

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿
+//#region 2-nin quvvetinin olub olmadigini tapan.
+//Console.WriteLine("Please choose number");
+//String numStr = Console.ReadLine();
+//int num = Convert.ToInt32(numStr);
+//while (num % 2 == 0)
+//{
+//    num = num / 2;
+//}
+//if (num == 1)
+//{
+//    Console.WriteLine("2-nin Quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("2-nin Quvveti deyil");
+//}
+
+#region X<Y<Z
+//Console.WriteLine("X:");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Y:");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Z:");
+//int z = Convert.ToInt32(Console.ReadLine());
+//if (x < y && x < z) ;
+//{
+//    Console.WriteLine("Z && Y Beraberdir");
+//}
+//else if (x > y && x > z)
+//{
+//    Console.WriteLine("X en boyukdur");
+//}
+//else if (y > z && y > x)
+//{
+//    Console.WriteLine("Y en boyukdur");
+//}
+//else
+//{
+//    Console.WriteLine("Z en boyukdur");
+//}
+//#endregion
+
+////Console.WriteLine("Birinci ededi girin:");
+////int num1 = Convert.ToInt32(Console.ReadLine());
+////Console.WriteLine("Ikinci ededi girin:");
+////int num2 = Convert.ToInt32(Console.ReadLine());
+////Console.WriteLine("-----------------------");
+
+
+////for (int i = num1; i < num2; i++)
+////{
+////    Console.WriteLine(i);
+////}
+///
+
+#region Verilen 2 eded arasindaki reqemleri ekrana yazdiran.
+//Console.WriteLine("Birinci ededi girin:");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ikinci ededi girin:");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("-----------------------");
+
+
+//for (int i = num1; i < num2; i++)
+//{
+//    Console.WriteLine(i);
+//}
+#endregion
